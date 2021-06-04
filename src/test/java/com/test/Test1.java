5d8280f6f074097b8b88eb90be65aab9ec8a1e29
unnikrishnanks-1 <unnikrishnanks1234@gmail.com>
2021-06-04 16:30:30 +0530

    second commit

---
+++ src/test/java/com/test/Test1.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class Test1 {
+    public static void test1(){
+        System.out.println("Hello World");
+    }
+
+
+    public static void main(String []args){
+        test1();
+    }
+}
