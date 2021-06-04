dbaa069a45d2e21b97f53818776468f626e830e1
unnikrishnanks-1 <unnikrishnanks1234@gmail.com>
2021-06-04 22:35:13 +0530

    newbranch commit

---
+++ src/test/java/com/test/Test2.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class Test2 {
+    public static void test1(){
+        System.out.println("Hello");
+    }
+
+
+    public static void main(String []args){
+        test1();
+    }
+}
